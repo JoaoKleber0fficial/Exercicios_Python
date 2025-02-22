3c77384fc987b439d792ded5778d034d71b5a3b8
JoaoKleber0fficial <ninjec.6650@gmail.com>
2025-02-22 16:21:31 -0300

    Atvs de Aritmética

---
+++ Exercicios_Aritmetica/atv4.py
@@ -0,0 +1,6 @@
+pi = 3.14
+raio = float(input('Coloque o Raio, para saber a área do ciruclo: '))
+
+areaCirculo = 3.14 * (raio ** 2)
+
+print(f'A área do circulo é: {areaCirculo}cm')
---
+++ Exercicios_Aritmetica/atv5.py
@@ -0,0 +1,9 @@
+polegadas = float(input('Coloque o valor desejado: '))
+pes = polegadas / 12
+jardas = pes / 3
+milhas = jardas / 1760
+
+print(f'Polegadas: {polegadas}')
+print(f'Pés: {pes}')
+print(f'Jardas: {jardas}')
+print(f'Milhas: {milhas}')
